ade613e5e5cb69d83ea287249eb9b9afe528ef55
netrobaselimited <netrobase@gmail.com>
2024-11-09 07:41:11 +0100

    Update About Page

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -13,25 +13,15 @@ const About = () => {
         <p className="text-white text-justify font-poppins text-2xl font-normal leading-normal">
           Kralis is a revolutionary School Result Management System (SRMS) that streamlines
           administrative tasks, enhances student engagement, and fosters academic success.
-          Designed to address the shortcomings of traditional paper-based and existing SRMS
-          systems, Kralis offers a comprehensive solution for educational institutions of all
-          sizes.
-          <br />
-          <br />
-          Here are some of the limitations Kralis aims to overcome:
-          <ul>
-            <li>Limited customization options for report formats and templates.</li>
-            <li>Initial setup and implementation may require technical expertise.</li>
-            <li>Lack of real-time updates and notifications for students, parents, and teachers.</li>
-            <li>Lack of integration with other educational tools like CBT, LMS, etc.</li>
-            <li>Difficulty in handling non-standard grading systems or academic structures.</li>
-            <li>Potential learning curve for users unfamiliar with digital result management systems.</li>
-          </ul>
-          <br />
-          These limitations motivated us to provide a robust, user-friendly solution. Whether
-          you are a school administrator seeking to modernize exam processes or a student striving
-          for academic excellence, Kralis is your comprehensive solution.
         </p>
+        <p className="text-white text-justify font-poppins text-2xl font-normal leading-normal mt-4">
+          Here are some of the limitations Kralis aims to overcome:
+        </p>
+        <ul className="list-disc list-inside text-white text-justify font-poppins text-2xl font-normal leading-normal mt-4">
+          <li>Limited customization options for report formats and templates.</li>
+          <li>Initial setup and implementation may require technical expertise.</li>
+          <li>Lack of real-time updates and notifications for students, parents, and teachers.</li>
+        </ul>
       </div>
     </section>
   );
